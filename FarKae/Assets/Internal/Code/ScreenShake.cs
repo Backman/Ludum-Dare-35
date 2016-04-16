baa8b8659122f8281ff8d15a4a18035d30b27733
Konrad Petersson <da_melta@hotmail.com>
2016-04-16 13:17:02 +0200

    Screenshake script on Camera
    Screenshake(float duration, float strength);

---
+++ FarKae/Assets/Internal/Code/ScreenShake.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+using DG.Tweening;
+
+public class ScreenShake : MonoBehaviour {
+
+	private Camera cam;
+
+
+	// Use this for initialization
+	void Start () {
+		cam = GetComponent<Camera> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	}
+
+	public void Screenshake (float duration = 0.05f, float strength = 0.05f){
+		cam.DOShakePosition (duration, strength, 10, 10f);
+
+	}
+
+}
